a8063efba1a3116d092b98e37ba446fd816c40fd
Jelle Keeris <jelle.keeris@gmail.com>
2019-07-16 10:44:38 +0200

    Commit & push Model.g.ts

---
+++ examples/src/Model.g.ts
@@ -0,0 +1,13 @@
+import { Tson } from "tson-runtime";
+/*
+    *** DO NOT EDIT! ***
+    Generated deserializable class from: Model.ts.
+*/
+@Tson
+class Model {
+    public static fromJson(data_1: any): Model {
+        const instance_1: Model = new Model();
+        return instance_1;
+    }
+}
+export default Model;
